5906cf8bda884c250ec4130b8a2108ef22a64017
Jason Whittle <jwhittle@bastiansolutions.com>
2017-09-11 09:30:06 -0400

    added comments to the .bat

--- install_zabbix_agent.bat
+++ install_zabbix_agent.bat
@@ -1,12 +1,14 @@
 @echo off
-#pull the Hostname from the system
+rem pull the Hostname from the system
 set HOSTNAME=%1
-#pull the ip address from the config file
+rem pull the ip address from the config file
 set IP=%2
+rem set path to zabbix install and .conf file
+set CONF_FILE=c:\zabbix\zabbix_agentd.conf
 
 ren C:\zabbix_agents_3.2.0.win zabbix
 
-#create .conf file
+rem create .conf file
 echo Server=%IP% > c:\zabbix\zabbix_agentd.conf
 echo ServerActive=%IP%:10051 > c:\zabbix\zabbix_agentd.conf
 echo Hostname=%HOSTNAME% >> c:\zabbix\zabbix_agentd.conf
@@ -17,7 +19,7 @@ echo Timeout=3 >> c:\zabbix\zabbix_agentd.conf
 call C:\zabbix\bin\win32\zabbix_agentd.exe --config c:\zabbix\zabbix_agentd.conf --install
 
 
-#update firewall
+rem update firewall
 set "fcmd=netsh advfirewall firewall add rule name="Zabbix Agent" dir=in action=allow program="C:\MyApp.exe" enable=yes profile="
 for %a in (domain public private) do (
     echo %fcmd%%a
@@ -29,6 +31,6 @@ netsh advfirewall firewall add rule name="MyApp" dir=in action=allow program="C:
 netsh advfirewall firewall add rule name="MyApp" dir=in action=allow program="C:\MyApp.exe" enable=yes profile=private
 pause
 
-#start the service
+rem start the service
 
-#verify all is well.
+rem verify all is well.
